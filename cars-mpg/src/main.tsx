082fc15fa52e34e39fa01e5235896f992cc050f1
Olgerd <olgerd.seviakov@ruptela.com>
2021-06-21 10:16:49 +0300

    move pages to separate files

--- cars-mpg/src/main.tsx
+++ cars-mpg/src/main.tsx
@@ -1,20 +1,8 @@
-import { FC } from 'react';
-import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom';
-import { Button, Table, Tabs, TBody, TD, THC, THead, TR, NavTab } from './components';
-import carsJson from './data/cars.json';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { NavTab, Tabs } from './components';
+import Home from './pages/home';
+import OurData from './pages/our-data';
 import styles from './main.module.scss';
-
-type Car = {
-  passedemissions: string;
-  mpg: number;
-  cylinders: number;
-  displacement: number;
-  horsepower: number;
-  weight: number;
-  acceleration: number;
-  modelyear: number;
-  carname: string;
-};
 
 const Main = () => (
   <Router>
@@ -37,45 +25,6 @@ const Main = () => (
       </Switch>
     </main>
   </Router>
-);
-
-const Home = () => (
-  <section>
-    <Button>click me</Button>
-  </section>
-);
-
-const OurData = () => (
-  <section>
-    <Table aria-label="our diagnosis data - cars">
-      <THead>
-        <THC>passedemissions</THC>
-        <THC className="mdc-data-table__header-cell--numeric">mpg</THC>
-        <THC className="mdc-data-table__header-cell--numeric">cylinders</THC>
-        <THC className="mdc-data-table__header-cell--numeric">displacement</THC>
-        <THC className="mdc-data-table__header-cell--numeric">horsepower</THC>
-        <THC className="mdc-data-table__header-cell--numeric">weight</THC>
-        <THC className="mdc-data-table__header-cell--numeric">acceleration</THC>
-        <THC className="mdc-data-table__header-cell--numeric">modelyear</THC>
-        <THC>carname</THC>
-      </THead>
-      <TBody>
-        {(carsJson as Car[]).map((car, index) => (
-          <TR key={index}>
-            <TD>{car.passedemissions}</TD>
-            <TD className="mdc-data-table__cell--numeric">{car.mpg}</TD>
-            <TD className="mdc-data-table__cell--numeric">{car.cylinders}</TD>
-            <TD className="mdc-data-table__cell--numeric">{car.displacement}</TD>
-            <TD className="mdc-data-table__cell--numeric">{car.horsepower}</TD>
-            <TD className="mdc-data-table__cell--numeric">{car.weight}</TD>
-            <TD className="mdc-data-table__cell--numeric">{car.acceleration}</TD>
-            <TD className="mdc-data-table__cell--numeric">{car.modelyear}</TD>
-            <TD>{car.carname}</TD>
-          </TR>
-        ))}
-      </TBody>
-    </Table>
-  </section>
 );
 
 export default Main;
---
+++ cars-mpg/src/pages/home.tsx
@@ -0,0 +1,9 @@
+import { Button } from '../components';
+
+const Home = () => (
+  <section>
+    <Button>click me</Button>
+  </section>
+);
+
+export default Home;
