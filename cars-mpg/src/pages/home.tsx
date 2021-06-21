d851822350fd436a0a5a6ae93351a38ce2dabba9
Olgerd <olgerd.seviakov@ruptela.com>
2021-06-21 16:52:54 +0300

    save before chart

--- cars-mpg/src/pages/home.tsx
+++ cars-mpg/src/pages/home.tsx
@@ -1,4 +1,46 @@
 import { Button } from '../components';
+import LinearRegression from './linear-regression';
+import carsJson from '../data/cars.json';
+import { Car } from '../types';
+
+const shuffle = require('lodash.shuffle');
+
+type ToUnion<T extends any[]> = T[number];
+const splitTest = 50;
+
+type DataRow = Array<[number, number, number]>;
+type LabelRow = Array<[number]>;
+const dataColumns = ['horsepower', 'weight', 'displacement'];
+const labelColumns = ['mpg'];
+
+const filterColumns = (columns: string[]) => (data: Car): string[] =>
+  columns.reduce((acc, key) => acc.concat((data as any)[key]), [] as string[]);
+
+const rawData = (carsJson as Car[]).slice(0, -1 * splitTest);
+const rawTestData = (carsJson as Car[]).slice(-1 * splitTest);
+const rawFeatures = (shuffle(rawData.map(filterColumns(dataColumns))) as any) as DataRow;
+const rawTestFeatures = (shuffle(rawTestData.map(filterColumns(dataColumns))) as any) as DataRow;
+const rawLabels = (shuffle(rawData.map(filterColumns(labelColumns))) as any) as LabelRow;
+const rawTestLabels = (shuffle(rawTestData.map(filterColumns(labelColumns))) as any) as LabelRow;
+
+const regression = new LinearRegression(rawFeatures, rawLabels, {
+  learningRate: 0.1,
+  iterations: 3,
+  batchSize: 10,
+});
+
+regression.train();
+const r2 = regression.test(rawTestFeatures, rawTestLabels);
+//
+// plot({
+//   x: regression.mseHistory.reverse(),
+//   xLabel: 'Iteration #',
+//   yLabel: 'Mean Squared Error',
+// });
+
+console.log('R2 is', r2);
+
+regression.predict([[120, 2, 380]]).print();
 
 const Home = () => (
   <section>
---
+++ cars-mpg/src/pages/linear-regression.ts
@@ -0,0 +1,111 @@
+import { Tensor } from '@tensorflow/tfjs';
+
+import * as tf from "@tensorflow/tfjs";
+
+type Options = { learningRate: number; iterations: number; batchSize: number };
+type RawFeatures = Array<[number, number, number]>;
+type RawLabels = Array<[number]>;
+
+class LinearRegression {
+  features: Tensor;
+  labels: Tensor;
+  mseHistory: number[];
+  options: Options;
+  weights: Tensor;
+  mean?: Tensor = undefined;
+  variance?: Tensor = undefined;
+
+  constructor(rawFeatures: RawFeatures, rawLabels: RawLabels, options: Options) {
+    this.features = this.processFeatures(rawFeatures);
+    this.labels = tf.tensor(rawLabels);
+    this.mseHistory = [];
+
+    this.options = Object.assign({ learningRate: 0.1, iterations: 1000 }, options);
+
+    this.weights = tf.zeros([this.features.shape[1] as number, 1]);
+  }
+
+  gradientDescent(features: Tensor, labels: Tensor) {
+    const currentGuesses = features.matMul(this.weights);
+    const differences = currentGuesses.sub(labels);
+
+    const slopes = features.transpose().matMul(differences).div(features.shape[0]);
+
+    this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
+  }
+
+  train() {
+    const batchQuantity = Math.floor(this.features.shape[0] / this.options.batchSize);
+
+    for (let i = 0; i < this.options.iterations; i++) {
+      for (let j = 0; j < batchQuantity; j++) {
+        const startIndex = j * this.options.batchSize;
+        const { batchSize } = this.options;
+
+        const featureSlice = this.features.slice([startIndex, 0], [batchSize, -1]);
+        const labelSlice = this.labels.slice([startIndex, 0], [batchSize, -1]);
+
+        this.gradientDescent(featureSlice, labelSlice);
+      }
+
+      this.recordMSE();
+      this.updateLearningRate();
+    }
+  }
+
+  predict(observations: RawFeatures) {
+    return this.processFeatures(observations).matMul(this.weights);
+  }
+
+  test(rawTestFeatures: RawFeatures, rawTestLabels: RawLabels) {
+    const testFeatures = this.processFeatures(rawTestFeatures);
+    const testLabels = tf.tensor(rawTestLabels);
+
+    const predictions = testFeatures.matMul(this.weights);
+
+    const sum$ = testLabels.sub(predictions).pow(2).sum();
+    const res = sum$.bufferSync().get();
+    const tot = testLabels.sub(testLabels.mean()).pow(2).sum().bufferSync().get();
+
+    return 1 - res / tot;
+  }
+
+  processFeatures(rawFeatures: RawFeatures): Tensor {
+    const features = tf.tensor(rawFeatures);
+    const featuresWith1 = tf.ones([features.shape[0], 1]).concat(features, 1);
+
+    return this.standardize(featuresWith1);
+  }
+
+  standardize(features: Tensor) {
+    if (!this.mean || !this.variance) {
+      const { mean, variance } = tf.moments(features, 0);
+
+      this.mean = mean;
+      this.variance = variance;
+    }
+
+    return features.sub(this.mean).div(this.variance.pow(0.5));
+  }
+
+  recordMSE() {
+    const mse$ = this.features.matMul(this.weights).sub(this.labels).pow(2).sum().div(this.features.shape[0])
+    const mse = mse$.bufferSync().get();
+
+    this.mseHistory.unshift(mse);
+  }
+
+  updateLearningRate() {
+    if (this.mseHistory.length < 2) {
+      return;
+    }
+
+    if (this.mseHistory[0] > this.mseHistory[1]) {
+      this.options.learningRate /= 2;
+    } else {
+      this.options.learningRate *= 1.05;
+    }
+  }
+}
+
+export default LinearRegression;
